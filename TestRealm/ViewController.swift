6c73dafdee7dd69822cdb02a0ef0cb2940f7ed3e
aLeSangK <le.sang@access-company.com>
2020-05-30 14:53:05 +0900

    Initial Commit

---
+++ TestRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRealm
+//
+//  Created by 楽桑 on 2020/05/30.
+//  Copyright © 2020 楽桑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
